afb1b6a30cc146c2b5b450532d2e5341d6309255
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2025-03-07 01:52:56 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+
+export default function App() {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <div className="p-4 bg-gray-100">
+    <App />
+  </div>
+)
