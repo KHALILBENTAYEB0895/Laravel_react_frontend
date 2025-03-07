2c2c711d027454ea0469124f354c030fb7eb9318
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2025-03-07 02:43:23 +0000

    Create Layout page

---
+++ src/Pages/Layout.jsx
@@ -0,0 +1,18 @@
+import { Outlet, Link } from "react-router-dom";
+
+export default function Layout() {
+    return(
+       <>
+        <header>
+            <nav>
+                <Link to="/" className="nav-link">Home</Link>
+            </nav>
+        </header>
+
+        <main>
+            <Outlet />
+        </main>
+       </>
+
+    )   
+}
