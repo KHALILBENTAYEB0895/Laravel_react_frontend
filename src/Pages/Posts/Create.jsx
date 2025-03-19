6cb7573056601dbf131383f80b2de6724e74f72b
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2025-03-19 23:45:25 +0000

    Add page for creat a Post

---
+++ src/Pages/Posts/Create.jsx
@@ -0,0 +1,16 @@
+export default function Create(){
+    return(
+        <>
+            <h1>Create a new Post</h1>
+            <form action="">
+                <div>
+                    <input type="text" placeholder="Post Title" />
+                </div>
+                <div>
+                    <textarea rows="6" placeholder="Post Content"></textarea>
+                </div>
+                <button className="primary-btn">Create</button>
+            </form>
+        </>
+    );
+}
