b51a8514be1c51a9fa865c6dfd8edb02c0f9488e
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2025-03-19 23:44:02 +0000

    Fix a User value bug using AppContext in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ import Register from './Pages/Auth/Register'
 import Login from './Pages/Auth/Login'
 import { useContext } from 'react'
 import { AppContext } from './Context/AppContext'
+import Create from './Pages/Posts/Create'
 
 export default function App() {
-const user = useContext(AppContext)
+const {user} = useContext(AppContext)
 
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />} >
           <Route index element={<Home />}/>
-          <Route path="register" element={!user ? <Home/> : <Register />} />
-          <Route path="login" element={!user ? <Home/> :<Login />} />
+          <Route path="register" element={user ? <Home/> : <Register />} />
+          <Route path="login" element={user ? <Home/> : <Login />} />
+          <Route path="create" element={user ? <Create/> : <Login/>} />
         </Route>
       </Routes>
     </BrowserRouter>
