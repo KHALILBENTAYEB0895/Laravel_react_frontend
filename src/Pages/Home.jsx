a4bfa39568f85a18b4723c3dbd678e9d7676f604
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2025-03-24 22:43:55 +0000

    Display Posts in Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,45 @@
-import { useContext } from "react"
-import { AppContext } from "../Context/AppContext"
+import { useEffect, useState } from "react";
+import { data } from "react-router-dom";
 
 export default function Home() {
 
-    // const {name} = useContext(AppContext)
+    const [posts, setPosts] = useState([]);
+
+    async function getPosts(){
+        const res = await fetch("/api/posts");
+        const data = await res.json();
+        
+        console.log(data);
+        
+        if(res.ok){
+            setPosts(data)
+        }
+    }
+
+    useEffect(() => {
+        getPosts();
+    },[])
 
     return(
-        <div>
-            <h1 className="title">Latest Posts of {name} !</h1>
-        </div>
+        <>
+            <h1 className="title">Latest Posts</h1>
+
+            {posts.length > 0 ? posts.map(post => (
+                <div key={post.id} className="mb-4 p-4 border rounded-md border-dashed border-slate-400">
+                    <div className="mb-2 flex items-start justify-between">
+                        <div>
+                            <h2 className="font-bold text-2xl">{post.title}</h2>
+                            <small className="text-xs text-slate-600">
+                                Create by {post.user.name}{" "} 
+                                on{" :"} {new Date(post.created_at)
+                                .toLocaleTimeString()}
+                            </small>
+                        </div>
+                        {/* here !!! */}
+                    </div>
+                    <p>{post.body}</p>
+                </div>
+            )) : <p>No posts found</p> }
+        </>
     )
 }
