36f1351945865cc5a84f9702c5531b1409ae711f
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2025-03-24 22:44:37 +0000

    Set up Create page

--- src/Pages/Posts/Create.jsx
+++ src/Pages/Posts/Create.jsx
@@ -1,13 +1,18 @@
 import { useContext, useState } from "react";
 import { AppContext } from "../../Context/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Create(){
+
+    const navigate = useNavigate();
+    
+    const [errors, setErrors] = useState({});
 
     const {token} = useContext(AppContext);
 
     const [formData, setFormData] = useState({
         title:"",
-        content:""
+        body:""
     });
 
     async function handleCreate(e){
@@ -21,7 +26,15 @@ export default function Create(){
             body: JSON.stringify(formData),
         });
 
-        console.log(formData);
+        const data = await res.json();
+
+
+        if(data.errors){
+            setErrors(data.errors);
+        }else{
+            navigate('/')
+        }
+
     }
 
     return(
@@ -33,13 +46,16 @@ export default function Create(){
                      placeholder="Post Title"
                      value={formData.title}
                      onChange={(e)=>setFormData({...formData, title: e.target.value})}/>
+                     {errors.title && <p className="error">{errors.title[0]}</p>}
                 </div>
+                
                 <div>
                     <textarea rows="6"
                      placeholder="Post Content"
                      value={formData.content}
-                     onChange={(e)=>setFormData({...formData, content: e.target.value})}>
+                     onChange={(e)=>setFormData({...formData, body: e.target.value})}>
                     </textarea>
+                    {errors.body && <p className="error">{errors.body[0]}</p>}
                 </div>
                 <button className="primary-btn">Create</button>
             </form>
