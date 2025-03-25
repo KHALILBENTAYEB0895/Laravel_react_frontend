465eeae0c006dc726d044e93d423c428609b7ca7
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2025-03-25 23:49:22 +0000

    Implement logic for Show Page

--- src/Pages/Posts/Show.jsx
+++ src/Pages/Posts/Show.jsx
@@ -1,7 +1,50 @@
-import { useParams } from "react-router-dom"
+import { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom"
+import { AppContext } from "../../Context/AppContext";
 
 export default function Show(){
-    console.log(useParams())
+
+    const {id} = useParams();
+    const {user} = useContext(AppContext);
+
+    const [post, setPost] = useState(null)
+
+    async function getPost(){
+        const res = await fetch(`/api/posts/${id}`);
+        const data = await res.json();
+
+        console.log(data);
+
+        if(res.ok){
+            setPost(data.post);
+        }
+    }
+
+    useEffect(() =>{
+        getPost();
+    },[])
+    
     return<>
+        {post ? 
+            <div key={post.id} className="mt-4 p-4 border rounded-md border-dashed border-slate-400">
+                <div className="mb-2 flex items-start justify-between">
+                    <div>
+                        <h2 className="font-bold text-2xl">{post.title}</h2>
+                        <small className="text-xs text-slate-600">
+                            Create by {post.user.name}{" "} 
+                            on{" :"} {new Date(post.created_at)
+                            .toLocaleTimeString()}
+                        </small>
+                    </div>
+                </div>
+                <p>{post.body}</p>
+                {user.id === post.user_id && 
+                <div className="flex items-center justify-end gap-4">
+                <Link to={`/posts/update/${post.id}`} className="bg-green-500 text-white text-sm rounded-lg px-3 py-1">update</Link>
+                </div>
+                }
+                {/* Here 22:50 */}
+            </div>
+         : <p className="title">Post not found !</p> }
     </>
 }
